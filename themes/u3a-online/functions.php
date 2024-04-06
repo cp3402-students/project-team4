dc248cf3608060e0a60354ac268f4a049b899ed0
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2024-04-06 19:41:41 +1000

    Added footer styling to theme

--- themes/u3a-online/functions.php
+++ themes/u3a-online/functions.php
@@ -46,10 +46,11 @@ function u3a_online_setup() {
 		*/
 	add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
+	// This theme uses wp_nav_menu() in two locations.
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'u3a-online' ),
+			'menu-2' => esc_html__( 'Secondary', 'u3a-online' ),
 		)
 	);
 
