1a8f34abb799c61460ea92c4f2c27857677f0289
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2024-04-18 19:40:46 +1000

    Update files for staging tests

--- themes/u3a-online/inc/customizer.php
+++ themes/u3a-online/inc/customizer.php
@@ -11,10 +11,12 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function u3a_online_customize_register( $wp_customize ) {
+	// Add postMessage support for site title and description.
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
+	// Partial refresh for site title and description.
 	if ( isset( $wp_customize->selective_refresh ) ) {
 		$wp_customize->selective_refresh->add_partial(
 			'blogname',
@@ -31,66 +33,61 @@ function u3a_online_customize_register( $wp_customize ) {
 			)
 		);
 	}
-	// Create Customizing Button Settings in Customizer.
+
+	// Create Button Settings section.
 	$wp_customize->add_section( 'buttons_section', array(
 		'title'    => __( 'Button Settings', 'u3a_online' ),
 		'priority' => 30,
 	) );
 
-	// Initialize Default Value for Custom Buttons.
-	// Button #1.
+	// Button #1 Text.
 	$wp_customize->add_setting( 'button_1_text', array(
 		'default'           => 'Become A Member',
 		'sanitize_callback' => 'sanitize_text_field',
 	) );
 
-	// Button #2.
+	// Button #2 Text.
 	$wp_customize->add_setting( 'button_2_text', array(
 		'default'           => 'Log In',
 		'sanitize_callback' => 'sanitize_text_field',
 	) );
 
-	// Initialize Default Links for Custom Buttons.
-	//Button #1.
+	// Button #1 Href.
 	$wp_customize->add_setting( 'button_1_href', array(
 		'default'           => '',
 		'sanitize_callback' => 'esc_url_raw',
 	) );
+	$wp_customize->add_control( 'button_1_href', array(
+		'label'   => __( 'Button #1 Href', 'u3a_online' ),
+		'section' => 'buttons_section',
+		'type'    => 'text',
+	) );
 
-	// Button #2.
+	// Button #2 Href.
 	$wp_customize->add_setting( 'button_2_href', array(
 		'default'           => '',
 		'sanitize_callback' => 'esc_url_raw',
 	) );
-
-
-	// Allow Href for Custom Buttons to be Set to a page within WordPress site.
-	// Button #1.
-	$wp_customize->add_control( 'button_1_href', array(
-		'label'   => __( 'Membership Button Href', 'u3a_online' ),
-		'section' => 'buttons_section',
-		'type'    => 'dropdown-pages',
-	) );
-
-	// Button #2.
 	$wp_customize->add_control( 'button_2_href', array(
-		'label'   => __( 'Log In Button Href', 'u3a_online' ),
+		'label'   => __( 'Button #2 Href', 'u3a_online' ),
 		'section' => 'buttons_section',
-		'type'    => 'dropdown-pages',
+		'type'    => 'text',
 	) );
-
 }
 
 add_action( 'customize_register', 'u3a_online_customize_register' );
 
+// Render site title.
 function u3a_online_customize_partial_blogname() {
 	bloginfo( 'name' );
 }
 
+// Render site description.
 function u3a_online_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
+// Enqueue script for live preview.
 function u3a_online_customize_preview_js() {
 	wp_enqueue_script( 'u3a-online-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
